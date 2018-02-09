ee2f0006c68b210fedd6f087b418ae7d5d015df8
Arun Gupta <arun.gupta@gmail.com>
2018-02-08 23:52:36 -0800

    using MediaType class instead of literal

--- services/greeting/src/main/java/org/aws/samples/compute/greeting/GreetingEndpoint.java
+++ services/greeting/src/main/java/org/aws/samples/compute/greeting/GreetingEndpoint.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author Arun Gupta
@@ -14,7 +15,7 @@ import javax.ws.rs.Produces;
 public class GreetingEndpoint {
 
     @GET
-    @Produces({"text/plain"})
+    @Produces({MediaType.TEXT_PLAIN})
     public String get() {
         return "Hello";
     }
