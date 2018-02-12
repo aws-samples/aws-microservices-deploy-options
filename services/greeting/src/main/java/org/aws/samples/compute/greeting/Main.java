0dada8acff5529d281f68d526236dd3f93ff76d2
Arun Gupta <arun.gupta@gmail.com>
2018-02-11 21:19:25 -0800

    removing Main class

--- services/greeting/src/main/java/org/aws/samples/compute/greeting/Main.java
+++
@@ -1,9 +0,0 @@
-package org.aws.samples.compute.greeting;
-
-import org.wildfly.swarm.Swarm;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new Swarm().start().deploy();
-    }
-}
--- services/name/src/main/java/org/aws/samples/compute/name/Main.java
+++
@@ -1,9 +0,0 @@
-package org.aws.samples.compute.name;
-
-import org.wildfly.swarm.Swarm;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        new Swarm().start().deploy();
-    }
-}
