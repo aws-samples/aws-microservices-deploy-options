e4b52a2b5850bbef82a747247e2b66d706f8b747
Ghosh <tapodg@186590cd90d5.ant.amazon.com>
2018-03-10 10:08:46 -0500

    Updated readme and infrastrucure.sh to provide push-button CloudFormation infra deploy

--- apps/ecs/ecs-cli/infrastructure.sh
+++ apps/ecs/ecs-cli/infrastructure.sh
@@ -2,16 +2,25 @@
 ######   Prerequisites for running this script   ######
 ######    Need awscli configured with default profile and  jq (https://github.com/stedolan/jq/wiki/Installation)   and perl 5 or higher#########
 
-OUTPUT_FILE="ecs-cluster.prop"
-
 if [ -f "$OUTPUT_FILE" ]; then
    echo "Removing prior property file"
    mv -f "$OUTPUT_FILE" "$OUTPUT_FILE".prior
 fi
 
-aws cloudformation create-stack --stack-name ecs-demo --template-body file://infra.yaml --capabilities CAPABILITY_IAM
+STACK_NAME=ecs-demo #default cluster name
+OUTPUT_FILE="ecs-cluster.prop"
+# if no command line arg given
+if [ -z $1 ]
+then
+  	echo "No stackname was passed, will launch a new CFN with name ecs-demo"
+	aws cloudformation create-stack --stack-name ecs-demo --template-url 'https://s3.amazonaws.com/compute-options-public/ecscli/infra.yaml' --capabilities CAPABILITY_IAM
+elif [ -n $1 ]
+then
+	echo -e "Setting CFN Stack name to $1"
+  	STACK_NAME=$1
+fi
 
-cloudformation_tail() {
+cfn_tail_create_new() {
   local stack="$1"
   local region="$2"
   local lastEvent
@@ -31,6 +40,8 @@ cloudformation_tail() {
 	
 	#[[ $stackStatus == *""* ]] || [[ $stackStatus == *"CREATE_FAILED"* ]] || [[ $stackStatus == *"COMPLETE"* ]]; do
 	lastEvent=$(aws cloudformation describe-stack-events --region $region --stack $stack --query 'StackEvents[].{ EventId: EventId, LogicalResourceId:LogicalResourceId, ResourceType:ResourceType, ResourceStatus:ResourceStatus, Timestamp: Timestamp }' --max-items 1 | jq .[0])
+	#check for typos in CFN Stack name from User
+	 [[  -z  $lastEvent  ]] && echo "Error while calling DescribeStacks" && exit 0
 	eventId=$(echo "$lastEvent" | jq -r .EventId)
 	if [ "$eventId" != "$lastEventId" ]
 	then
@@ -39,8 +50,7 @@ cloudformation_tail() {
 	fi
 	sleep 3
 	stackStatus=$(aws cloudformation describe-stacks --region $region --stack-name $stack | jq -c -r .Stacks[0].StackStatus)
-
-       if [ "$stackStatus" == "CREATE_COMPLETE" ]
+       if [[ "$stackStatus" == "CREATE_COMPLETE" ]]
        then
            aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[0].Outputs' --output=text | tee ecs-cluster.prop       
        fi 
@@ -48,9 +58,9 @@ cloudformation_tail() {
     done
   echo "Stack Status: $stackStatus"
 }
+cfn_tail_create_new $STACK_NAME us-east-1
 
-cloudformation_tail ecs-demo us-east-1
-
+#Create property file
 if [ -f "$OUTPUT_FILE" ]; then
 	perl -i.bak -lpe 's/\s+/=/g' "$OUTPUT_FILE"
 	echo -e "Created Output property file"
