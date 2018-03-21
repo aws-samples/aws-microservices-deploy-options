8541e0ff0e6707ad12f09914d1f48661218978e4
Arun Gupta <arun.gupta@gmail.com>
2018-03-20 17:17:28 -0700

    adding logging and X-Ray segments

--- services/greeting/src/main/java/org/aws/samples/compute/greeting/StartupBean.java
+++ services/greeting/src/main/java/org/aws/samples/compute/greeting/StartupBean.java
@@ -4,6 +4,8 @@ import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.plugins.ECSPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
@@ -11,12 +13,16 @@ import javax.inject.Singleton;
 @Singleton
 public class StartupBean {
 
+    private static final Logger logger = LoggerFactory.getLogger(StartupBean.class);
+
     @PostConstruct
     public void setup() {
+        logger.info("StartupBean.setup.entry");
         AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder
                 .standard()
                 .withPlugin(new EC2Plugin()).withPlugin(new ECSPlugin());
 
         AWSXRay.setGlobalRecorder(builder.build());
+        logger.info("StartupBean.setup.exit");
     }
 }
